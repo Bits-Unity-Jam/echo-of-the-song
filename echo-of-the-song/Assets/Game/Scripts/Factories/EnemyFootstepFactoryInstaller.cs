a2d0c9d459b7724657776d42b17990e60958d7db
disco <this.disco.w@gmail.com>
2022-10-30 11:46:23 +0300

    enemy footsteps

---
+++ echo-of-the-song/Assets/Game/Scripts/Factories/EnemyFootstepFactoryInstaller.cs
@@ -0,0 +1,18 @@
+﻿using Game.Scripts.Footsteps;
+using UnityEngine;
+using Zenject;
+
+namespace Game.Scripts.Factories
+{
+    public class EnemyFootstepFactoryInstaller : MonoInstaller
+    {
+        [ SerializeField ]
+        private EnemyFootstepFactory simpleDebrisFactory;
+
+        public override void InstallBindings() => BindObjectFactory();
+
+        private void BindObjectFactory() =>
+            Container.Bind<IFactory<EnemyFootstep>>()
+                .FromMethod(() => simpleDebrisFactory.GetComponent<IFactory<EnemyFootstep>>()).AsSingle();
+    }
+}
