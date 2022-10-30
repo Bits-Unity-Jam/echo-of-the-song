0a212ec21db4f095d05736c3e5cb644c5c20a4fc
Byndiu <semenbindu@gmail.com>
2022-10-30 03:48:10 +0300

    LoseScreen

--- echo-of-the-song/Assets/Game/Scripts/Moves/Player.cs
+++ echo-of-the-song/Assets/Game/Scripts/Moves/Player.cs
@@ -1,13 +1,16 @@
+using System;
 using UnityEngine;
 using Zenject;
 
 namespace Game.Scripts.Moves
 {
-    public class Player : MonoBehaviour
+    public class Player : MonoBehaviour, IDiable
     {
         private IMoveable<Vector2WorldSpaceData> _moveable;
 
         private MovementVectorPresenter _movementVectorPresenter;
+
+        public event Action OnDie;
 
         [ Inject ]
         private void Construct(IMoveable<Vector2WorldSpaceData> moveable,
@@ -24,6 +27,20 @@ namespace Game.Scripts.Moves
         private void HandleNewMovementDirection(Vector2WorldSpaceData movementVector)
         {
             _moveable.HandleDirection(movementVector);
+        }
+
+        public void Die()
+        {
+            OnDie.Invoke();
+            Destroy(gameObject);
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag == "Enemy")
+            {
+                Die();
+            }
         }
     }
 }
